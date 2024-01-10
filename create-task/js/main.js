dc0830acdc02de3d12ab49282bb7bf7afacb4764
pintongc <pintongc@nycstudents.net>
2024-01-10 09:23:38 -0500

    1

---
+++ create-task/js/main.js
@@ -0,0 +1,16 @@
+import { menu } from "./nutritionfacts";
+
+const calperpro = menu.forEach((item) => console.log(item.protein / item.calories))
+console.log(calperpro);
+
+const calories = 1094
+const protein = 56
+
+function math(calories, protein){
+    const a = calories / protein;
+    console.log(a);
+    return[a];
+};
+
+math(calories, protein);
+
---
+++ create-task/js/nutritionfacts.js
@@ -0,0 +1,7 @@
+const menu = [
+    {item: "Egg McMuffin", calories: 100, fat: 199, protein: 1999},
+
+];
+
+export {menu};
+
