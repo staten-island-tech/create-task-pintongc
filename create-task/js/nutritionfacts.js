eb0abd4731ba3eb62afd47ebceacb07b5d80190e
pintongc <pintongc@nycstudents.net>
2024-01-11 09:23:09 -0500

    2

--- create-task/js/nutritionfacts.js
+++ create-task/js/nutritionfacts.js
@@ -1,6 +1,14 @@
 const menu = [
-    {item: "Egg McMuffin", calories: 100, fat: 199, protein: 1999},
-
+    {item: "Egg McMuffin", calories: 310, fat: 13, protein: 17, price: 1.00},
+    {item: "Bacon Egg and Cheese Biscuit", calories: 460, fat: 26, protein: 17, price: 1.00},
+    {item: "Sausage McMuffin", calories: 400, fat: 26, protein: 14},
+    {item: "Sausage McMuffin with Egg", calories: 480, fat: 31, protein: 20, price: 1.00},
+    {item: "Egg McMuffin", calories: 100, fat: 199, protein: 1999, price: 1.00},
+    {item: "Egg McMuffin", calories: 100, fat: 199, protein: 1999, price: 1.00},
+    {item: "Egg McMuffin", calories: 100, fat: 199, protein: 1999, price: 1.00},
+    {item: "Egg McMuffin", calories: 100, fat: 199, protein: 1999, price: 1.00},
+    {item: "Egg McMuffin", calories: 100, fat: 199, protein: 1999, price: 1.00},
+    
 ];
 
 export {menu};
