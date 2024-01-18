e517ce9f1f0197cd9e53c1d2d688ef92f480628d
pintongc <pintongc@nycstudents.net>
2024-01-18 09:23:25 -0500

    6

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -16,14 +16,30 @@ math(calories, protein);
 
 function caloverprice(calories, price){
     let b = calories / price; 
-    console.log(b)
+    let c = Math.round(b * 100) / 100
+    console.log(c)
+    document.querySelector("h1").insertAdjacentHTML("beforeend", `<p>${c}</p>`)
 }
 caloverprice(1400, 5.99)
 
 function asdasd(){
-    if (document.querySelector(".option")) {
-        
-    } else {
-        
+    if ((document.querySelector(".option") = "calories")) {
+        caloverprice()
     }
+};
+
+function prooverprice(protein, price){
+    let b = protein / price; 
+    let c = Math.round(b * 100) / 100
+    console.log(c)
+    document.querySelector("h1").insertAdjacentHTML("beforeend", `<p>${c}</p>`)
 }
+prooverprice();
+
+function proovercals(protein, calories){
+    let b = protein / calories; 
+    let c = Math.round(b * 100) / 100
+    console.log(c)
+    document.querySelector("h1").insertAdjacentHTML("beforeend", `<p>${c}</p>`)
+}
+proovercals();
